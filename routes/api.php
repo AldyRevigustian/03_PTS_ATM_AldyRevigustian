8386fc6daa3e0f034d087f4491805dec0f03a221
AldyRevigustian <aldyrevig@gmail.com>
2022-09-20 12:05:42 +0700

    Noomer 5

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', [TransactionController::class, 'cek']);
+
+Route::post('tarik_tunai', [TransactionController::class, 'tarik_tunai']);
+Route::post('cek_saldo', [TransactionController::class, 'cek_saldo']);
+Route::post('pembayaran', [TransactionController::class, 'pembayaran']);
